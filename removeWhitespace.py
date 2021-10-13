60b50e7f425f0c9e7ecf638457a166ac11daf6a5
tails618 <asher.grossman@gmail.com>
2021-10-12 20:15:21 -0500

    removeWhitespace complete

--- removeWhitespace.py
+++ removeWhitespace.py
@@ -3,9 +3,26 @@ from sys import argv
 f = open(argv[1])
 fo = open(argv[2], "w")
 outtext = ""
-whitespace = [" ","	",]
+whitespace = [" ","	"]
 lines=[]
+blankIndexes=[]
+finalList=[]
+counter = 0
 
 for i in f:
 	lines.append(i)
-print(lines)
+	if i == "\n":
+		blankIndexes.insert(0,counter)
+	counter += 1
+for i in blankIndexes:
+	lines.pop(i)
+for i in lines:
+	for j in i:
+		if not j in whitespace:
+			finalList.append(j)
+if finalList[len(finalList) - 1] == '' or finalList[len(finalList) - 1] == '\n':
+	finalList.pop(len(finalList) - 1)
+
+finalString = ''.join(finalList)
+
+fo.write(finalString)
