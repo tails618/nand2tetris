24986c66e08b130e6eaf0bac45888e22adf9d9d7
tails618 <asher.grossman@gmail.com>
2021-11-01 21:42:59 -0500

    Fill and Mult I think

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,57 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//Stores the end location of the screen in the memory
+@SCREEN
+D=A
+@8192
+D=D+A
+@screenend
+M=D
+//Stores the current location of the screen in memory
+@SCREEN
+D=A
+@screencurrent
+M=D
+//Checks if a key is being held down
+(CHECKKEY)
+    @KBD
+    D=M
+    @SCREENFILL
+    D;JNE
+    @SCREENEMPTY
+    D;JEQ
+//If a key is being held down and the current location in memory is less than the end location of the screen, then turn on pixels.
+//Otherwise, go back to CHECKKEY
+(SCREENFILL)
+    @screenend
+    D=M
+    @screencurrent
+    D=D-M
+    @CHECKKEY
+    D;JEQ
+    //At this point, the current location in memory is less than the end location of the screen, so it turns on pixels.
+    @screencurrent
+    A=M
+    M=1
+    @screencurrent
+    M=M+1
+    @CHECKKEY
+    0;JMP
+//If not, turn off pixels as long as the current location in memory is greater than/equal to the first location.
+(SCREENEMPTY)
+    @screencurrent
+    D=M
+    @SCREEN
+    D=D-A
+    @CHECKKEY
+    D;JEQ
+    //At this point, the current location in memory is greater than the start location of the screen, so it turns off pixels.
+    @screencurrent
+    A=M
+    M=0
+    @screencurrent
+    M=M-1
+    @CHECKKEY
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,38 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+//HOW IT WORKS: Sets i to R1, loops adding R0 to R2 and decrements i by one each time, until i=0.
+//Stores R1 in D
+@R1
+D=M
+//Creates counter, sets to R1, adds to RAM at i
+@i
+M=D
+//Creates location in RAM for result
+@result
+M=0
+//Main loop where adding happens
+(MAIN)
+    //Checks if i is 0
+    @i
+    D=M
+    @END
+    D;JEQ
+    //Adds R0 to R2
+    @R0
+    D=M
+    @result
+    M=D+M
+    //Decrements i
+    @i
+    M=M-1
+    //Goes back to beginning of loop
+    @MAIN
+    0;JMP
+
+//Ends program
+(END)
+    @result
+    D=M
+    @R2
+    M=D
