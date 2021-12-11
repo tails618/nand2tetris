775823b0fbcff9c38c9fd8aee3462337e856a01f
tails618 <asher.grossman@gmail.com>
2021-12-10 22:06:03 -0600

    i changed this

--- removeWhitespace.py
+++ removeWhitespace.py
@@ -2,27 +2,41 @@ from sys import argv
 
 f = open(argv[1])
 fo = open(argv[2], "w")
-outtext = ""
-whitespace = [" ","	"]
+
 lines=[]
-blankIndexes=[]
-finalList=[]
-counter = 0
+linesToRemove=[]
 
+#read the file line by line
 for i in f:
+	#adds the line to a list of lines
 	lines.append(i)
-	if i == "\n":
-		blankIndexes.insert(0,counter)
-	counter += 1
-for i in blankIndexes:
-	lines.pop(i)
-for i in lines:
-	for j in i:
-		if not j in whitespace:
-			finalList.append(j)
-if finalList[len(finalList) - 1] == '' or finalList[len(finalList) - 1] == '\n':
-	finalList.pop(len(finalList) - 1)
 
-finalString = ''.join(finalList)
+#remove spaces and tabs from strings in lines
+counter = 0
+for i in lines:
+	if i.find(' ') != -1 or i.find('	') != -1:
+		lines[counter] = i.replace(" ","")
+		lines[counter] = i.replace("	","")
+	else:
+		counter += 1
+
+#if the line is blank or a comment, add it to a list of lines to remove
+#we cant remove the line here, because that would disrupt the for loop
+counter = 0
+for i in lines:
+	if i == "\n" or i.startswith("//"):
+		linesToRemove.insert(0,counter)
+	#if the line contains a comment but has code prior to the comment, remove the comment
+	if i.find("//") != -1:
+		lines[counter] = i[:i.find("//")]
+		lines[counter] = lines[counter] + "\n"
+	counter += 1
+
+#removes the lines from the list of lines
+for i in linesToRemove:
+	lines.pop(i)
+
+#convert the list to a string
+finalString = ''.join(lines)
 
 fo.write(finalString)
