06515aeacac674d025b73117a0cb0f095a8e352e
tails618 <asher.grossman@gmail.com>
2022-01-18 11:30:58 -0600

    Changed writing functionality to write from string

--- removeWhitespace2.py
+++ removeWhitespace2.py
@@ -5,7 +5,6 @@ fo = open(argv[2], "w")
 
 #recursive function to remove whitespace and comments
 def stripLine(line):
-    print(line)
     firstChar = line[0]
     #if the line is blank or a comment, return an empty string.
     if firstChar == '\n' or firstChar == '/':
@@ -23,16 +22,18 @@ def stripLine(line):
 #creates a list of the lines in the file, and a counter variable
 lines = f.readlines()
 counter = 0
+finalString = ''
 
 #for each line in the file, strip the whitespace and comments, and write the result to the output file. Also, add newlines to the output file, EXCEPT the final line.
-for i in f:
+for i in lines:
     if i != '\n' and i != ' ' and i != '\t' and i != '':
         strippedLine = stripLine(i)
         if strippedLine != '':
-            fo.write(strippedLine)
+            finalString = finalString + strippedLine
             if counter != len(lines) - 1:
-                fo.write('\n')
+                finalString = finalString + '\n'
     counter += 1
 
+fo.write(finalString)
 f.close()
 fo.close()
